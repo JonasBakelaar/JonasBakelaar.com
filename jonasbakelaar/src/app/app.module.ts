eb54171129f73bd2f58b3ec6e70a3bcec0641df0
Jonas Bakelaar <jonas.bakelaar@emmetros.com>
2019-01-08 18:43:24 -0500

    Site deployment with router
    Just gotta add content to the site and a better UI

--- jonasbakelaar/src/app/app.module.ts
+++ jonasbakelaar/src/app/app.module.ts
@@ -1,18 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { CoopComponent } from './coop/coop.component';
 import { UiModule } from './ui/ui.module';
+import { HomeComponent } from './home/home.component';
+
+const appRoutes: Routes = [
+  { path: 'coop', component: CoopComponent },
+  {
+    path: 'home',
+    component: HomeComponent,
+    data: { title: 'Jonas Bakelaar | Testing' }
+  },
+  { path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  { path: '**', component: HomeComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    CoopComponent
+    CoopComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    UiModule
+    UiModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
